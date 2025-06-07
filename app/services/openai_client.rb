b497d83b116354e06500cd89ddbf1d71946eb75c
rina87 <hanchm0303@gmail.com>
2025-06-07 15:20:15 +0900

    job,クライアント作成

---
+++ app/services/openai_client.rb
@@ -0,0 +1,23 @@
+require "openai"
+
+class OpenaiClient # OpenAIとの接続クラス
+  def initialize
+    @client = OpenAI::Client.new(access_token: Rails.application.credentials.openai[:api_key])
+  end
+
+  def generate_response(prompt)
+    response = @client.chat(
+      parameters: {
+        model: "gpt-4o-mini",
+        messages: [ { role: "user", content: prompt } ]
+      }
+    )
+    ai_message = response.dig("choices", 0, "message", "content")
+
+  unless ai_message
+    Rails.logger.error("OpenAI response failed or empty: #{response.inspect}")
+  end
+
+  ai_message
+  end
+end
