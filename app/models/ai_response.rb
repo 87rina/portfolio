c806204d257d2b2f97c4a79112bcf4da1e325f88
rina87 <hanchm0303@gmail.com>
2025-06-07 13:55:15 +0900

    add:Posts,AiResponsesモデル生成

---
+++ app/models/ai_response.rb
@@ -0,0 +1,2 @@
+class AiResponse < ApplicationRecord
+end
---
+++ db/migrate/20250607045149_create_ai_responses.rb
@@ -0,0 +1,10 @@
+class CreateAiResponses < ActiveRecord::Migration[7.2]
+  def change
+    create_table :ai_responses do |t|
+      t.text :content, null: false
+      t.references :post, foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
