d3cdc451e4338a315c091c11f2eeaf9860979d6d
rina87 <hanchm0303@gmail.com>
2025-06-15 13:35:50 +0900

    フォントの変更

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['"Yusei Magic"', 'sans-serif'],
       },
       backgroundImage: {
         'hero-pattern': "url('/assets/background.jpg')", 
