665ef83b17eb051b439e984a7689a38b67235a69
rina87 <hanchm0303@gmail.com>
2025-06-19 14:44:07 +0900

    lintチェック修正

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,12 +1,11 @@
 module IconHelper
-    def inline_svg(name, options = {})
-      file_path = Rails.root.join("app/assets/icons/#{name}.svg")
-      return "(icon missing)" unless File.exist?(file_path)
-  
-      svg = File.read(file_path)
-      doc = Nokogiri::HTML::DocumentFragment.parse(svg)
-      svg_tag = doc.at_css "svg"
-      svg_tag["class"] = options[:class] if options[:class]
-      doc.to_html.html_safe
-    end
+  def inline_svg(name, options = {})
+    file_path = Rails.root.join("app/assets/icons/#{name}.svg")
+    return "(icon missing)" unless File.exist?(file_path)
+    svg = File.read(file_path)
+    doc = Nokogiri::HTML::DocumentFragment.parse(svg)
+    svg_tag = doc.at_css "svg"
+    svg_tag["class"] = options[:class] if options[:class]
+    doc.to_html.html_safe
   end
+end
