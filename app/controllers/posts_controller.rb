c97e0e3b6519dc0de3622b0f63a42c86ec5d120d
rina87 <hanchm0303@gmail.com>
2025-06-22 22:52:05 +0900

    記録累計数の表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
+
+    # 合計件数
+    @total_posts_count = @posts.count
+
+    # 日付だけの配列をユニークに取得（最新順）
+    recorded_dates = @posts.pluck(:created_at).map(&:to_date).uniq.sort.reverse
+
+    # 連続日数の計算
+    @consecutive_days = calculate_consecutive_days(recorded_dates)
+  end
+
+  private
+
+  def calculate_consecutive_days(dates)
+    return 0 if dates.empty?
+
+    count = 1
+    (1...dates.length).each do |i|
+      if dates[i] == dates[i - 1] - 1
+        count += 1
+      else
+        break
+      end
+    end
+    count
   end
 end
