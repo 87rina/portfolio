7cd056be4e9d0cdfdecd8b8b74a025c103dd6f57
rina87 <hanchm0303@gmail.com>
2025-06-23 02:40:37 +0900

    lintチェック修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,10 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
-
     # 合計件数
     @total_posts_count = @posts.count
-
-    # 日付だけの配列をユニークに取得（最新順）
+    # 連続記録日数
     recorded_dates = @posts.pluck(:created_at).map(&:to_date).uniq.sort.reverse
-
-    # 連続日数の計算
     @consecutive_days = calculate_consecutive_days(recorded_dates)
   end
 
