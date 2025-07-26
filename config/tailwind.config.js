e6162d32bb612b47974788b9d7d00013abf94bca
rina87 <hanchm0303@gmail.com>
2025-07-27 05:24:48 +0900

    フォント変更

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Yusei Magic"', 'sans-serif'],
+        sans: ["Kiwi Maru", "serif"], 
       },
       backgroundImage: {
         'hero-pattern': "url('/assets/background.jpg')", 
