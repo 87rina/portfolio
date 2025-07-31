ac9964cf1be17d038a34393bac6da80aceb311c0
rina87 <hanchm0303@gmail.com>
2025-08-01 03:31:41 +0900

    書き方に困ったら

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -1,10 +1,19 @@
 class DescriptionsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :how_character_selection ]
-  # キャラクター変更機能の説明
+  skip_before_action :authenticate_user!, only: [ :how_character_selection, :how_to_write ]
+  # 「キャラクター変更」
   def how_character_selection
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: turbo_stream.append("character-selection", partial: "shared/character_selection")
+      end
+    end
+  end
+
+  # 「書き方に困ったら」
+  def how_to_write
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.append("how-to-write", partial: "shared/how_to_write")
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   get "conversation_history", to: "conversations#show_history"
   # 各種説明文
   get "how_character_selection", to: "descriptions#how_character_selection", as: :how_character_selection
+  get "how_to_write", to: "descriptions#how_to_write", as: :how_to_write
   resources :chats, only: [ :index, :create ]
   resources :posts, only: [ :index, :show, :destroy ]
 end
