97abecd1eeeea0a96816b3a2d74bc1cfaff701d1
rina87 <hanchm0303@gmail.com>
2025-08-14 07:57:34 +0900

    ActiveStorageとimage_processingの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :ai_responses, through: :posts
   belongs_to :character, optional: true
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
