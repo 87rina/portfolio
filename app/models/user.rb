ecb7d06a3357c4e66da9d5cd5026ee66b3fbb1e1
rina87 <hanchm0303@gmail.com>
2025-08-18 04:53:16 +0900

    avatarのリサイズ

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,24 @@ class User < ApplicationRecord
   has_many :ai_responses, through: :posts
   belongs_to :character, optional: true
   has_one_attached :avatar
+
+  before_save :resize_avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+  # 保存時に画像サイズをリサイズしてS3へのアップロード容量を制御
+  def resize_avatar
+    return unless avatar.attached?
+
+    avatar.variant(resize_to_limit: [ 400, 400 ]).processed.tap do |variant|
+      avatar.attach(
+        io: StringIO.new(variant.download),
+        filename: avatar.filename,
+        content_type: avatar.content_type
+      )
+    end
+  end
 end
