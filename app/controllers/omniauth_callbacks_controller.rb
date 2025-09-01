4ad5079402baa9c366c6a01210b0367a86765cb3
rina87 <hanchm0303@gmail.com>
2025-09-02 05:22:20 +0900

    カラム追加、コントローラー・モデル編集

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,3 +1,33 @@
-class OmniauthCallbacksController < ApplicationController
-  def line; ; end
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def line; basic_action end
+
+  private
+  def basic_action
+    @omniauth = request.env["omniauth.auth"]  # OmniAuth が返してきた認証情報を取得
+    if @omniauth.present?
+      # SNSアカウントと対応するユーザーを探す
+      @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
+      if @profile.email.blank?
+        # メールアドレスが設定されていない場合の処理
+        # OmniAuthから email が取得できない場合はダミーアドレス（uid-provider@example.com）を作成
+        email = @omniauth["info"]["email"] ? @omniauth["info"]["email"] : "#{@omniauth["uid"]}-#{@omniauth["provider"]}@example.com"
+        # すでにcurrent_userがある場合はそのユーザーにSNS情報を紐づける。ない場合は新しくUserを作成
+        @profile = current_user || User.create!(
+                    provider: @omniauth["provider"],
+                    uid: @omniauth["uid"],
+                    email: email,
+                    name: @omniauth["info"]["name"],
+                    password: Devise.friendly_token[0, 20]
+                  )
+      end
+      @profile.set_values(@omniauth) # set_values メソッドを呼び出し、トークンや追加情報を保存
+      sign_in(:user, @profile) # Deviseの機能で、ログイン処理を実行
+    end
+    flash[:notice] = "ログインしました"
+    redirect_to root_path
+  end
+
+  def fake_email(uid, provider)
+    "#{uid}-#{provider}@example.com"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[ line ]
 
   private
+  # <プロフィール>
   # 保存時に画像サイズをリサイズしてS3へのアップロード容量を制御
   def resize_avatar
     return unless avatar.attached?
@@ -26,5 +27,26 @@ class User < ApplicationRecord
         content_type: avatar.content_type
       )
     end
+  end
+
+  # <LINEログイン>
+  # SNS連携データを取得
+  def social_profile(provider)
+    social_profiles.select { |sp| sp.provider == provider.to_s }.first
+  end
+  # OmniAuth から受け取った認証情報をモデルの属性にセット
+  def set_values(omniauth)
+    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+    credentials = omniauth["credentials"]
+    info = omniauth["info"]
+    access_token = credentials["refresh_token"]
+    access_secret = credentials["secret"]
+    credentials = credentials.to_json
+    name = info["name"]
+  end
+  # SNSから取得した「ユーザープロフィール情報（raw_info）」をJSON形式で保存しDBに即時反映
+  def set_values_by_raw_info(raw_info)
+    self.raw_info = raw_info.to_json
+    self.save!
   end
 end
