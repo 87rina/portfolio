60387d388c5edd4cb1cab04735d2050461d0b342
rina87 <hanchm0303@gmail.com>
2025-09-10 22:37:22 +0900

    コントローラーとビューの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = current_user.posts.order(created_at: :desc).page(params[:page])
-    # 合計件数
-    @total_posts_count = @posts.count
-    # 連続記録日数
-    recorded_dates = @posts.pluck(:created_at).map(&:to_date).uniq.sort.reverse
-    @consecutive_days = calculate_consecutive_days(recorded_dates)
+    @user = current_user
+    @posts = @user.posts.order(created_at: :desc).page(params[:page])
   end
 
   def show
@@ -17,21 +13,5 @@ class PostsController < ApplicationController
     post = current_user.posts.find(params[:id])
     post.destroy!
     redirect_to posts_path, notice: t("defaults.flash_message.deleted", item: Post.model_name.human), status: :see_other
-  end
-
-  private
-
-  def calculate_consecutive_days(dates)
-    return 0 if dates.empty?
-
-    count = 1
-    (1...dates.length).each do |i|
-      if dates[i] == dates[i - 1] - 1
-        count += 1
-      else
-        break
-      end
-    end
-    count
   end
 end
