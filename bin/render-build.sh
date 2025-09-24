b55bcabc122add7f5797b11631ca18ad9cc49361
rina87 <hanchm0303@gmail.com>
2025-09-25 08:30:09 +0900

    db初期データ投入

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rails db:migrate
+rails db:seed RAILS_ENV=production #　本番環境のDBに初期データを投入
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset # DBリセット
