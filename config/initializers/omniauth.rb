3fc40f5cf0cf6f1c60b874eb19e8add51c8724f6
rina87 <hanchm0303@gmail.com>
2025-10-16 23:02:59 +0900

    config/initializers/omniauth.rb削除

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :line,
-           Rails.application.credentials.dig(:line, :key),
-           Rails.application.credentials.dig(:line, :secret),
-           scope: "profile openid email",
-           redirect_uri: "#{ENV['APP_URL']}/users/auth/line/callback"
-end
